313b74a203cc139e2a8e1a0c01b56fb86f2bc9c8
Sandoche Adittane <sandoche@gmail.com>
2017-09-17 23:41:38 +0200

    index.js fixed

--- _src/index.js
+++ _src/index.js
@@ -8,36 +8,41 @@ import 'vanilla-tilt';
 // CSS and SASS files
 import './index.scss';
 
-const options = {
-  modal: '.modal',
-  modalInner: '.modal-inner',
-  modalContent: '.modal-content',
-  open: '[data-modal-open]',
-  close: '[data-modal-close]',
-  page: 'body',
-  loadClass: 'vanilla-modal',
-  class: 'modal-visible',
-  clickOutside: false,
-  closeKeys: [27],
-  transitions: true,
-  onBeforeOpen: null,
-  onBeforeClose: null,
-  onOpen: null,
-  onClose: null,
-};
-const modal = new VanillaModal(options);
-const scroll = new SmoothScroll('.smooth');
+if(window.location.pathname === '/') {
 
-window.sr = ScrollReveal({ viewOffset: { top: 0, right: 0, bottom: 100, left: 0 }, delay: 200 });
-sr.reveal('.book__title');
-sr.reveal('.book__description');
-sr.reveal('.how__row');
-sr.reveal('.step', { duration: 500 }, 200);
-sr.reveal('.tokens__title');
-sr.reveal('.tokens__description');
-sr.reveal('.token', { duration: 500 }, 200);
-sr.reveal('.calendar__title');
-sr.reveal('.calendar__timeline');
-sr.reveal('.team__container');
 
-particlesJS.load('particles-js', '/particles.json', function() {});
+  const options = {
+    modal: '.modal',
+    modalInner: '.modal-inner',
+    modalContent: '.modal-content',
+    open: '[data-modal-open]',
+    close: '[data-modal-close]',
+    page: 'body',
+    loadClass: 'vanilla-modal',
+    class: 'modal-visible',
+    clickOutside: false,
+    closeKeys: [27],
+    transitions: true,
+    onBeforeOpen: null,
+    onBeforeClose: null,
+    onOpen: null,
+    onClose: null,
+  };
+  const modal = new VanillaModal(options);
+  const scroll = new SmoothScroll('.smooth');
+
+  window.sr = ScrollReveal({ viewOffset: { top: 0, right: 0, bottom: 100, left: 0 }, delay: 200 });
+  sr.reveal('.book__title');
+  sr.reveal('.book__description');
+  sr.reveal('.how__row');
+  sr.reveal('.step', { duration: 500 }, 200);
+  sr.reveal('.tokens__title');
+  sr.reveal('.tokens__description');
+  sr.reveal('.token', { duration: 500 }, 200);
+  sr.reveal('.calendar__title');
+  sr.reveal('.calendar__timeline');
+  sr.reveal('.team__container');
+
+  particlesJS.load('particles-js', '/particles.json', function() {});
+
+}
