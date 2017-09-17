cec861abddbd64bca411a86faa5542c8620ebdef
Sandoche Adittane <sandoche@gmail.com>
2017-09-17 10:31:02 +0200

    new design updated

--- _src/index.js
+++ _src/index.js
@@ -36,7 +36,7 @@ sr.reveal('.step', { duration: 500 }, 200);
 sr.reveal('.tokens__title');
 sr.reveal('.tokens__description');
 sr.reveal('.token', { duration: 500 }, 200);
-sr.reveal('.team');
+sr.reveal('.team__container');
 
 
 particlesJS.load('particles-js', '/particles.json', function() {});
